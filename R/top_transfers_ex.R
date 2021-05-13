d00401266514c7957da6b41de75a2b2818ba64ec
Julie Zhang <juliezhang@Julies-MacBook-Pro.local>
2021-05-12 19:04:57 -0700

    fit the SOM model and looked at the clusters

--- R/top_transfers_ex.R
+++ R/top_transfers_ex.R
@@ -1,10 +1,13 @@
 library(kohonen)
 
-top_transfers_leagues <- read.csv('~/Documents/STATS305C-SOM/data/top_transfers_all.csv')
+all_df <- read.csv('~/Documents/STATS305C-SOM/data/top_transfers_all.csv')
 # drop the index from reading in 
-top_transfers_leagues <- top_transfers_leagues[,-c(1, 4,5,6,7)]
-top_transfers_leagues$Season <- as.factor(top_transfers_leagues$Season)
+top_transfers_leagues <- all_df[,-c(1,2, 5,6,7,8)]
 head(top_transfers_leagues)
+
+top_transfers_leagues[, c(1,3,7,8)] <- lapply(top_transfers_leagues[, c(1,3,7,8)], factor)
+head(top_transfers_leagues)
+lapply(top_transfers_leagues, summary)
 
 # just try it out 
 fit_som_models <- function(df) {
@@ -33,7 +36,7 @@ fit_som_models <- function(df) {
   som_codebook <- som_model$codes[[1]]
   
   cols <- c('red', 'blue', 'green', 'yellow', 'black')
-  plot(som_model, type='mapping', keepMargins = F, col=NA, 
+  plot(som_model, type='mapping', keepMargins = T, col=NA, 
        bg= cols[som_hc], add.cluster.boundaries(som_model, som_hc))
   
   # mapping nodes back
@@ -42,22 +45,42 @@ fit_som_models <- function(df) {
   
   # Heatmap for specific variable 
   
-  par(mfrow=c(3,3))
+  par(mfrow=c(2,2))
   for (i in 1:ncol(som_codebook)) {
     plot(som_model, type='property', property= som_codebook[,i],
          main= colnames(som_codebook)[i], palette.name = colorRampPalette(c('blue', 'green', 'yellow','red')))
     add.cluster.boundaries(som_model, som_hc)
-
   }
+  
+  return(df)
 }
 
-fit_som_models(subset(top_transfers_leagues, select=-c(Price_diff)))
+# try with the market value and transfer fee, again transfer fee is really driving it 
+top_transfers_leagues1 <- subset(top_transfers_leagues, select=-c(Price_diff))
+top_transfers_leagues1 <- fit_som_models(top_transfers_leagues1)
+
+# cluster #5 has only 5 transfers! they happen to be the 5 transfers with 
+# largest transfer fee and largest market value 
+sum(top_transfers_leagues1$cluster == 5)
+all_df[which(top_transfers_leagues1$cluster == 5), ]
+sort(all_df$Transfer_fee, decreasing = T)[1:5]
+sort(all_df$Market_value, decreasing = T)[1:21]
 
 # without season 
 top_transfers_leagues2 <- subset(top_transfers_leagues, select=-c(Season, Price_diff))
 fit_som_models(top_transfers_leagues2)
 
+# try with only the price difference 
 top_transfers_leagues3 <- subset(top_transfers_leagues, select=-c(Market_value, Transfer_fee))
-fit_som_models(top_transfers_leagues3)
+head(top_transfers_leagues3)
+summary(top_transfers_leagues3)
 
-               
+top_transfers_leagues3 <- fit_som_models(top_transfers_leagues3)
+par(mfrow=c(1,1))
+hist(top_transfers_leagues3$cluster)
+
+# cluster #5 has only 4 transfers! they happen to be the 4 transfers with largest price gap 
+which(top_transfers_leagues3$cluster == 5)
+all_df[which(top_transfers_leagues3$cluster == 5), ]
+sort(all_df$Price_diff, decreasing = T)[1:5]
+
